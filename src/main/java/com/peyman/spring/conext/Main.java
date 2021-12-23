43346f669860479651918e22a6792c02c3073c42
peyma <peymandodangeh@gmail.com>
2021-12-23 09:29:29 +0330

    adding context config file

--- src/main/java/com/peyman/spring/conext/Main.java
+++ src/main/java/com/peyman/spring/conext/Main.java
@@ -1,4 +1,13 @@
 package com.peyman.spring.conext;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Student student = (Student) context.getBean("student");
+        System.out.println(student);
+    }
 }
---
+++ src/main/java/com/peyman/spring/conext/Student.java
@@ -0,0 +1,16 @@
+package com.peyman.spring.conext;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Student {
+
+    private int age;
+    private String name;
+    private String family;
+
+}
